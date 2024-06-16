2dde1e23fe921ee926dd6c385ffaa492b9c4a410
Lillium <lilliumbruno@gmail.com>
2024-06-15 21:19:07 -0700

    Remove quit handler and put functions in UserInterface

--- Scenes/SceneLoader.gd
+++ Scenes/SceneLoader.gd
@@ -1,2 +1,12 @@
 extends Node
 
+
+@export var SceneToLoad : PackedScene
+@export var SceneName: String
+
+func Unload():
+
+func Load():
+    var newScene = SceneToLoad.instantiate()
+    newScene.set_meta("SCN_NAME", SceneName) 
+    GameConstants.Instance.add_sibling(newScene)
--- Scripts/UserInterface.gd
+++ Scripts/UserInterface.gd
@@ -10,8 +10,6 @@ static var INSTANCE : UserInterface
 @export var SettingsMenuScreen : SettingsMenu
 @export var MainTextFont : Font
 
-
-
 func _ready():
 	INSTANCE = self
 	SettingsMenuScreen.Load()
@@ -25,3 +23,24 @@ func _process(_delta):
 
 func go_to_menu():
 	pass
+
+
+
+func ExitGame(): 
+	if (!is_inside_tree()): return
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
+
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		var file = FileAccess.open("user://README.txt", FileAccess.WRITE)
+		file.store_line("Thank you for playing our game! We hope you enjoyed it!")
+		file.store_line("Developers:")
+		file.store_line("- Feintha")
+		file.store_line("- AresRF")
+		file.store_line("Artists:")
+		file.store_line("- TBD (Probably not fei lol)")
+		file.store_line("Storywriters:")
+		file.store_line("- AresRF")
+		file.store_line("- Feintha")
+		file.store_line("- Ice909")
