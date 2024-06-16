debb3495a3d4c454750cf93d971ed36f16ad029f
Lillium <lilliumbruno@gmail.com>
2024-06-15 21:40:39 -0700

    yippe no paini this time

--- Scenes/SceneLoader.gd
+++ Scenes/SceneLoader.gd
@@ -5,27 +5,27 @@ class_name SceneLoader extends Node
 @export var SceneName: String
 
 func Unload():
-    for child in GameConstants.Instance.get_parent().get_children():
-        if (child.has_meta("SCN_NAME") && child.get_meta("SCN_NAME") == SceneName):
-            child.queue_free()
+	for child in GameConstants.Instance.get_parent().get_children():
+		if (child.has_meta("SCN_NAME") && child.get_meta("SCN_NAME") == SceneName):
+			child.queue_free()
 func UnloadAllRuntimeScenes():
-    for child in GameConstants.Instance.get_parent().get_children():
-        if child.has_meta("SCN_LOADED_AT_RUNTIME") && child.has_meta("SCNMGR_DISABLE_AUTO_UNLOAD"):
-            child.queue_free()
+	for child in GameConstants.Instance.get_parent().get_children():
+		if child.has_meta("SCN_LOADED_AT_RUNTIME") && !child.has_meta("SCNMGR_DISABLE_AUTO_UNLOAD"):
+			child.queue_free()
 func Load():
-    LoadFromScene(SceneToLoad)
+	LoadFromScene(SceneToLoad)
 func LoadFromScene(scene:PackedScene):
-    var newScene = scene
-    newScene.set_meta("SCN_NAME", SceneName) 
-    newScene.set_meta("SCN_LOADED_AT_RUNTIME", true) 
-    if (!shouldUnload):
-        newScene.set_meta("SCNMGR_DISABLE_AUTO_UNLOAD", true)
-    GameConstants.Instance.add_sibling(newScene)
+	var newScene = scene.instantiate()
+	newScene.set_meta("SCN_NAME", SceneName) 
+	newScene.set_meta("SCN_LOADED_AT_RUNTIME", true) 
+	if (!shouldUnload):
+		newScene.set_meta("SCNMGR_DISABLE_AUTO_UNLOAD", true)
+	GameConstants.Instance.add_sibling(newScene)
 func LoadFromPath(path:String):
-    var newScene = load(path)
-    LoadFromScene(newScene)
+	var newScene = load(path)
+	LoadFromScene(newScene)
 @export var shouldUnload : bool = true
 # * Bad name: Explanation::
-    # * If true, the scene spawned from this object will not be freed from UnloadAllRuntimeScenes. Unload() will still free regardless!
+	# * If true, the scene spawned from this object will not be freed from UnloadAllRuntimeScenes. Unload() will still free regardless!
 func SetShouldUnload(state:bool):
-    shouldUnload = state
+	shouldUnload = state
